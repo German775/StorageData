909077b34ac07d0724b5e525c3368d7852fd2c21
German Shkudov <german.shkudov@solbeg.com>
2019-08-01 21:26:23 +0300

    Last

--- StorageData/Controllers/GetBackgroundController.cs
+++ StorageData/Controllers/GetBackgroundController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StorageData.DBContext;
 using System.IO;
+using StorageData.TransferData;
 
 namespace StorageData.Controllers
 {
@@ -20,8 +21,21 @@ namespace StorageData.Controllers
             this.configuration = new Configuration();
         }
         [HttpPost]
-        public string Post(string backgroundId)
+        public IEnumerable<JsonBackground> Post(Guid eventId)
         {
+            var backgroundList = new List<JsonBackground>();
+            var background = new JsonBackground();
+
+            var framesId = dbContext.EventAttributes.Where(item => item.Frames.EventId == eventId && item.Parameters.Name == "Type" && item.Value == "Background").Select(item => item.Frames.Id);
+            foreach (var frame in framesId)
+            {
+                background.BackgroundId = dbContext.EventAttributes.Where(item => item.Parameters.Name == "BackgroundId" && Convert.ToString(item.Frames.Id) == Convert.ToString(frame)).Select(item => item.Value).First();
+                background.Data = 
+            }
+            return background;
+
+
+            /*
             var frameId = dbContext.EventAttributes.Where(item => item.Parameters.Name == "BackgroundId" && item.Value == backgroundId).Select(item => item.Frames.Id).First();
             var eventId = dbContext.EventAttributes.Where(item => item.Parameters.Name == "BackgroundId" && item.Value == backgroundId).Select(item => item.Frames.EventId).First();
             using (var fileStream = new FileStream($"{configuration.GetConfiguration().PathForStoreImage}//{eventId}//Background//{frameId}.jpg".ToString(), FileMode.Open, FileAccess.Read))
@@ -30,6 +44,7 @@ namespace StorageData.Controllers
                 fileStream.Read(buffer, 0, (int)fileStream.Length);
                 return (Convert.ToBase64String(buffer));
             };
+            */
         }
     }
 }
---
+++ StorageData/TransferData/JsonBackground.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageData.TransferData
+{
+    public class JsonBackground
+    {
+        public string BackgroundId { get; set; }
+        public string Data { get; set; }
+    }
+}
